e844436f1152930fdaa4651f7685ad7953849ed2
Ricardo Champa <Ricardo@MacBook-Pro-de-Ricardo.local>
2014-12-21 23:18:10 +0100

    fix it vars

--- .openshift/cron/minutely/certain_date.sh
+++ .openshift/cron/minutely/certain_date.sh
@@ -4,7 +4,7 @@ HOUR=`date +%H`
 MINUTE=`date +%M`
 
 
-if [ "DATE" == "12/21/14" -a "$HOUR" == "23" -a "$MINUTE" == "15" ];then
+if [ "$DATE" == "12/21/14" -a "$HOUR" == "23" -a "$MINUTE" == "19" ];then
 	(
             date >> $CUSTOM_REPO_DIR/certain_date.txt 
         )
