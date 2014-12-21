4bbd1b06604f185aab7d3a1411a0f2977b1dc051
Ricardo Champa <Ricardo@MacBook-Pro-de-Ricardo.local>
2014-12-21 23:39:01 +0100

    meh

--- .openshift/cron/minutely/certain_date.sh
+++ .openshift/cron/minutely/certain_date.sh
@@ -4,7 +4,7 @@ HOUR=`date +%H`
 MINUTE=`date +%M`
 
 
-if [ "$DATE" == "12/21/14" -a "$HOUR" == "17" -a "$MINUTE" == "37" ];then
+if [ "$DATE" == "12/21/14" -a "$HOUR" == "17" -a "$MINUTE" == "40" ];then
 	(
             date
         )
