3fe4518eb730c6b788b54ddc630a614e869bbac2
Ricardo Champa <Ricardo@MacBook-Pro-de-Ricardo.local>
2014-12-21 22:58:12 +0100

    testing or

--- .openshift/cron/minutely/test.sh
+++ .openshift/cron/minutely/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MINUTES=`date +%M`
 
-if [ "$MINUTES" == "55" ];then
+if [ "$MINUTES" == "59" -o "$MINUTES" == "01" ];then
 	(
             date >> $CUSTOM_REPO_DIR/test_date.txt 
         )
