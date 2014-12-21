3b4dd4dd9d442ca1a8087bca27095342e7ed1e32
Ricardo Champa <Ricardo@MacBook-Pro-de-Ricardo.local>
2014-12-21 23:57:08 +0100

    fucniona

--- .openshift/cron/minutely/certain_date.sh
+++ .openshift/cron/minutely/certain_date.sh
@@ -4,8 +4,8 @@ HOUR=`date +%H`
 MINUTE=`date +%M`
 
 
-if [ "$DATE" == "12/21/14" -a "$HOUR" == "17" -a "$MINUTE" == "40" ];then
+if [ "$DATE" == "12/21/14" -a "$HOUR" == "17" -a "$MINUTE" == "58" ];then
 	(
-            date
+            date >> $CUSTOM_REPO_DIR/date.txt
         )
 fi
