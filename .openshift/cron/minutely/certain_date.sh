1912b0a38bc1037061f7ce9f8f03e9d13d7aaddd
Ricardo Champa <Ricardo@MacBook-Pro-de-Ricardo.local>
2014-12-23 20:54:39 +0100

    clear

--- .openshift/cron/minutely/certain_date.sh
+++ .openshift/cron/minutely/certain_date.sh
@@ -4,7 +4,16 @@ HOUR=`date +%H`
 MINUTE=`date +%M`
 
 
-if [ "$DATE" == "12/23/14" -a "$HOUR" == "14" -a "$MINUTE" == "51" ];then
+if [ "$DATE" == "12/23/14" -a "$HOUR" == "14" -a "$MINUTE" == "56" ];then
+	(
+        #date >> $CUSTOM_REPO_DIR/certain_date.txt
+        cd $CUSTOM_REPO_DIR ; 
+        $CUSTOM_PHP cron_certain_date.php
+    )
+fi
+
+
+if [ "$DATE" == "12/23/14" -a "$HOUR" == "14" -a "$MINUTE" == "58" ];then
 	(
         #date >> $CUSTOM_REPO_DIR/certain_date.txt
         cd $CUSTOM_REPO_DIR ; 
